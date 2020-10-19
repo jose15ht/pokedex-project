4a1e4bb4de0a58e48b651c371a18fd4834517675
Jose Herrera <joseignacioht@gmail.com>
2020-10-19 12:49:55 -0300

    Finished base files of the project

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+$(function(){
+
+  $('button').click(function(event){
+    event.preventDefault();
+
+    var pokemonID = $('#pokedex').val()
+    var settings = {
+      "url": `https://pokeapi.co/api/v2/pokemon/${pokemonID}`,
+      "method": "GET",
+    };
+    
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      $('#pokeName').text(response.name)
+      $('img').attr('src', response.sprites['front_shiny'])
+      $('#pokeWeight').text(`Peso: ${response.weight} (Kg)`)
+      createGraph(response)
+    });
+  })
+
+  function createGraph(data){
+    var pokeStats = data.stats.map(function(about){
+      return { label: about.stat.name, y: about.base_stat }
+    })
+
+    var chart = new CanvasJS.Chart("chartContainer", {
+      theme: "light1", // "light2", "dark1", "dark2"
+      animationEnabled: true, // change to true		
+      title:{
+        text: "Estadísticas del Pokemón"
+      },
+      data: [
+      {
+        // Change type to "bar", "area", "spline", "pie",etc.
+        type: "column",
+        dataPoints: pokeStats
+        
+      }
+      ]
+    });
+    chart.render();
+  } 
+})
