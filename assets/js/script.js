191f439a59df7556ed0c42b121d8d136e3c66b0d
José Herrera <joseignacioht@gmail.com>
2021-07-31 22:47:11 -0400

    Agregada una verificacion para la solicitud del pokemon y actualizacion de aspectos visuales para una concordancia en el diseño de los elementos

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,20 @@ $(function(){
       "url": `https://pokeapi.co/api/v2/pokemon/${pokemonID}`,
       "method": "GET",
     };
+    //comprobacion para asegurarse que se ingrese un digito que exista en la api para los pokemones
+    if (parseInt(pokemonID) <= 0 || parseInt(pokemonID) > 898){
+      window.alert("Error en PokeID. Identificación fuera de rango; no  hay registros.")
+    } else if (pokemonID === ""){
+      window.alert("Debe ingresar un número para la consulta.")
+    } else {
+      $.ajax(settings).done(function (response) {
+        $('#pokeName').text(response.name)
+        $('#pokeSprite').attr('src', response.sprites['front_shiny'])
+        $('#pokeWeight').text(`Peso: ${response.weight} (Lb)`)
+        createGraph(response)
+      });
+    }
     
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-      $('#pokeName').text(response.name)
-      $('#pokeSprite').attr('src', response.sprites['front_shiny'])
-      $('#pokeWeight').text(`Peso: ${response.weight} (Lb)`)
-      createGraph(response)
-    });
   })
 
   function createGraph(data){
@@ -24,19 +30,28 @@ $(function(){
     })
 
     var chart = new CanvasJS.Chart("chartContainer", {
-      theme: "light1", // "light2", "dark1", "dark2"
+      //theme: "dark1", // "light2", "dark1", "dark2"
+      backgroundColor: "#343a40",
       animationEnabled: true, // change to true		
       title:{
-        text: "Estadísticas del Pokemón"
+        text: "Estadísticas del Pokemón",
+        fontColor: "#effd0d", // 1, color para concordar con el desplegadoen el sitio.
+      },
+      axisX:{
+        labelFontColor: "#effd0d" // 2
+      },
+      axisY:{
+        labelFontColor: "#effd0d" // 3
       },
       data: [
-      {
-        // Change type to "bar", "area", "spline", "pie",etc.
-        type: "column",
-        dataPoints: pokeStats
+        {
+          // Change type to "bar", "area", "spline", "pie",etc.
+          type: "column",
+          dataPoints: pokeStats
         
-      }
-      ]
+        }
+      ],
+      width: 600,
     });
     chart.render();
   } 
